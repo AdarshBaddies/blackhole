821b1e5b08581b243640fb61369f26978844c0be
adarshbaddies <adarshnothing578@gmail.com>
2026-01-21 21:22:33 +0530

    added the new golang backend, changed the flow of the frontend

---
+++ client/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Replace 'your-repo-name' with your actual repository name on GitHub
+  base: '/AdarshBaddies/', 
+  build: {
+    outDir: 'dist',
+  }
+})
